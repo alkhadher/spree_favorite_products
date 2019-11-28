489d427dca53ae10bff3498ce0f78bb3a5b16ac8
Khadher Alqiari <k.alqiari@floranow.com>
2019-11-28 12:52:20 +0200

    Update add_link_to_users_favorite_products.rb

--- app/overrides/add_link_to_users_favorite_products.rb
+++ app/overrides/add_link_to_users_favorite_products.rb
@@ -2,7 +2,6 @@ Deface::Override.new(
   virtual_path: "spree/shared/_main_nav_bar",
   name: "add_link_to_users_favorite_products",
   insert_after: "#home-link",
-  text: "<% if spree_current_user %><li><%= link_to 'Favorite Products', favorite_products_path %></li><% end %>",
+  text: "<% if spree_current_user %><li class='nav-item' data-hook=''><%= link_to 'Favorite Products', favorite_products_path, class: 'nav-link'%></li><% end %>",
   sequence: { before: "auth_shared_login_bar"}
 )
-
